02afc0ee5f23e2aa15f26e9d2af6f559bef06568
aqntks <cjd0030@gmail.com>
2021-12-19 17:00:20 +0900

    modify path

--- test.py
+++ test.py
@@ -1,143 +1,121 @@
+
+import numpy as np
+import pandas as pd
+
 import torch
-import torch.nn.functional as F
+from torch import nn
+from torch.utils.data import DataLoader
+from torchvision import *
+from torchvision.transforms import *
 import matplotlib.pyplot as plt
 
-from torch import nn
-from torch import Tensor
-from PIL import Image
-from torchvision.transforms import Compose, Resize, ToTensor
-from einops import rearrange, reduce, repeat
-from einops.layers.torch import Rearrange, Reduce
 from torchsummary import summary
+from vit import *
 
 
-# Patch Embedding
-class PatchEmbedding(nn.Module):
-    def __init__(self, in_channels: int = 3, patch_size: int = 16, emb_size: int = 768, img_size: int = 224):
-        self.patch_size = patch_size
-        super().__init__()
-        self.projection = nn.Sequential(
-            nn.Conv2d(in_channels, emb_size, kernel_size=patch_size, stride=patch_size),
-            Rearrange('b e (h) (w) -> b (h w) e')
-        )
-        self.cls_token = nn.Parameter(torch.randn(1, 1, emb_size))
-        self.positions = nn.Parameter(torch.randn((img_size // patch_size) **2 + 1, emb_size))
+# options
+batch_size = 8
+epochs = 20
 
-    def forward(self, x: Tensor) -> Tensor:
-        b, _, _, _ = x.shape
-        x = self.projection(x)
-        cls_tokens = repeat(self.cls_token, '() n e -> b n e', b=b)
-        x = torch.cat([cls_tokens, x], dim=1)
-        x += self.positions
+device = 'cuda:1' if torch.cuda.is_available() else 'cpu'
 
-        return x
+# augmentation
+transform = transforms.Compose([# transforms.RandomHorizontalFlip(),
+                                transforms.Resize((224, 224)),
+                                transforms.ToTensor()])
 
+# data load
+train_data = datasets.CIFAR10(root="data",
+                            train=True,
+                            download=True,
+                            transform=transform)
 
-# MHA(Multi Head Attention)
-class MultiHeadAttention(nn.Module):
-    def __init__(self, emb_size: int = 768, num_heads: int = 8, dropout: float = 0):
-        super().__init__()
-        self.emb_size = emb_size
-        self.num_heads = num_heads
-        self.qkv = nn.Linear(emb_size, emb_size * 3)
-        self.att_drop = nn.Dropout(dropout)
-        self.projection = nn.Linear(emb_size, emb_size)
+test_data = datasets.CIFAR10(root="data",
+                           train=False,
+                           download=True,
+                           transform=transform)
 
-    def forward(self, x: Tensor, mask: Tensor = None) -> Tensor:
-        qkv = rearrange(self.qkv(x), "b n (h d qkv) -> (qkv) b h n d", h=self.num_heads, qkv=3)
-        queries, keys, values = qkv[0], qkv[1], qkv[2]
-        energy = torch.einsum('bhqd, bhkd -> bhqk', queries, keys)
-        if mask is not None:
-            fill_value = torch.finfo(torch.float32).min
-            energy.mask_fill(~mask, fill_value)
-
-        scaling = self.emb_size ** (1/2)
-        att = F.softmax(energy, dim=-1) / scaling
-        att = self.att_drop(att)
-
-        out = torch.einsum('bhal, bhlv -> bhav', att, values)
-        out = rearrange(out, "b h n d -> b n (h d)")
-        out = self.projection(out)
-        return out
+# data loader
+train_dataLoader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
+test_dataLoader = DataLoader(test_data, batch_size=batch_size, shuffle=False)
 
 
-# Residual Block
-class ResidualAdd(nn.Module):
-    def __init__(self, fn):
-        super().__init__()
-        self.fn = fn
-
-    def forward(self, x, **kwargs):
-        res = x
-        x = self.fn(x, **kwargs)
-        x += res
-        return x
-
-
-# Feed Forward MLP
-class FeedForwardBlock(nn.Sequential):
-    def __init__(self, emb_size: int, expansion: int = 4, drop_p: float=0.):
-        super().__init__(
-            nn.Linear(emb_size, expansion * emb_size),
-            nn.GELU(),
-            nn.Dropout(drop_p),
-            nn.Linear(expansion * emb_size, emb_size),
+# define model
+class Model(nn.Module):
+    def __init__(self):
+        super(Model, self).__init__()
+        self.flatten = nn.Flatten()
+        self.linear_relu_stack = nn.Sequential(
+            nn.Linear(28*28*3, 512),
+            nn.ReLU(),
+            nn.Linear(512, 512),
+            nn.ReLU(),
+            nn.Linear(512, 10)
         )
 
-
-# Transformer Encoder Block
-class TransformerEncoderBlock(nn.Sequential):
-    def __init__(self,
-                 emb_size: int = 768,
-                 drop_p: float = 0.,
-                 forward_expansion: int = 4,
-                 forward_drop_p: float = 0.,
-                 ** kwargs):
-        super().__init__(
-            ResidualAdd(nn.Sequential(
-                nn.LayerNorm(emb_size),
-                MultiHeadAttention(emb_size, **kwargs),
-                nn.Dropout(drop_p)
-            )),
-            ResidualAdd(nn.Sequential(
-                nn.LayerNorm(emb_size),
-                FeedForwardBlock(
-                    emb_size, expansion=forward_expansion, drop_p=forward_drop_p),
-                nn.Dropout(drop_p)
-            )
-            ))
+    def forward(self, x):
+        x = self.flatten(x)
+        logits = self.linear_relu_stack(x)
+        return logits
 
 
-# TransformerEncoder
-class TransformerEncoder(nn.Sequential):
-    def __init__(self, depth: int = 12, **kwargs):
-        super().__init__(*[TransformerEncoderBlock(**kwargs) for _ in range(depth)])
+# create model
+# model = Model().to(device)
+model = ViT(img_size=224, n_classes=10).to(device)
 
 
-# Classification Head
-class ClassificationHead(nn.Sequential):
-    def __init__(self, emb_size: int = 768, n_classes: int = 1000):
-        super().__init__(
-            Reduce('b n e -> b e', reduction='mean'),
-            nn.LayerNorm(emb_size),
-            nn.Linear(emb_size, n_classes))
+# optimizer
+loss_fn = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
 
 
-# ViT
-class ViT(nn.Sequential):
-    def __init__(self,
-                 in_channels: int = 3,
-                 patch_size: int = 16,
-                 emb_size: int = 768,
-                 img_size: int = 224,
-                 depth: int = 12,
-                 n_classes: int = 1000,
-                 **kwargs):
-        super().__init__(
-            PatchEmbedding(in_channels, patch_size, emb_size, img_size),
-            TransformerEncoder(depth, emb_size=emb_size, **kwargs),
-            ClassificationHead(emb_size, n_classes)
-        )
+# train
+def train(dataLoader, model, loss_fn, optimizer):
+    size = len(dataLoader.dataset)
+    model.train()
+    for batch, (X, y) in enumerate(dataLoader):
+        X, y = X.to(device), y.to(device)
+
+        # Compute prediction error
+        pred = model(X)
+        loss = loss_fn(pred, y)
+
+        # Backpropagation
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if batch % 100 == 0:
+            loss, current = loss.item(), batch * len(X)
+            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
 
 
-summary(ViT(), (3, 224, 224), device='cpu')
+# test
+def eval(dataLoader, model, loss_fn):
+    size = len(dataLoader.dataset)
+    num_batches = len(dataLoader)
+    model.eval()
+    test_loss, correct = 0, 0
+    with torch.no_grad():
+        for X, y in dataLoader:
+            X, y = X.to(device), y.to(device)
+            pred = model(X)
+            test_loss += loss_fn(pred, y).item()
+            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+    test_loss /= num_batches
+    correct /= size
+    final_accuracy = f'{(100 * correct):>0.1f}'
+    print(f"Test Error: \n Accuracy: {(100 * correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+
+    return final_accuracy
+
+
+for t in range(epochs):
+    print(f"Epoch {t + 1}\n-------------------------------")
+    train(train_dataLoader, model, loss_fn, optimizer)
+    final_accuracy = eval(test_dataLoader, model, loss_fn)
+print("Done!")
+
+# save model
+torch.save(model.state_dict(), f"weights/model_ep{epochs}_ac{final_accuracy}.pth")
+print("Saved PyTorch Model State to model.pth")
