28831164cdebedf4c6c0613ba951e4e81f867a17
aqntks <cjd0030@gmail.com>
2022-01-27 12:04:20 +0900

    add argument

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import torch
 import torch.cuda
 import torch.nn as nn
-# from my_vit import ViT
-# from vit import ViT
-from vit2 import ViT
+import my_vit
+import vit
+import vit2
 from torchvision import transforms, datasets
+
+import argparse
 
 # 랜덤 계수
 random_seed = 17
@@ -57,9 +59,8 @@ def evaluate(model, test_loader, criterion, DEVICE):
     return test_loss, test_accuracy
 
 
-def main():
-    batch_size = 32
-    EPOCHS = 5
+def main(arg):
+    batch_size, EPOCHS, img_size, vit_num = arg.batch, arg.epoch, arg.img, arg.vit
 
     if torch.cuda.is_available():
         device = torch.device('cuda')
@@ -67,7 +68,7 @@ def main():
         device = torch.device('cpu')
     print('PyTorch 버전:', torch.__version__, ' Device:', device)
 
-    trans = transforms.Compose([transforms.Resize((256, 256)),
+    trans = transforms.Compose([transforms.Resize((img_size, img_size)),
                                 # transforms.RandomHorizontalFlip(),
                                 transforms.ToTensor(),
                                 transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
@@ -94,22 +95,25 @@ def main():
         print('y_train:', y_train.size(), 'type:', y_train.type())
         break
 
-    # myvit
-    # model = ViT(img_size=32, n_classes=10).to(device)
-    # vit
-    # model = ViT(image_size=32, patch_size=16, num_classes=10, dim=768, depth=12, heads=12, mlp_dim=3072).to(device)
+    if vit_num == 1:
+        # myvit
+        model = my_vit.ViT(img_size=img_size, n_classes=10).to(device)
+    elif vit_num == 2:
+        # vit
+        model = vit.ViT(image_size=img_size, patch_size=16, num_classes=10, dim=768, depth=12, heads=12, mlp_dim=3072).to(device)
     # vit2
-    model = ViT(
-        image_size=256,
-        patch_size=32,
-        num_classes=1000,
-        dim=1024,
-        depth=6,
-        heads=16,
-        mlp_dim=2048,
-        dropout=0.1,
-        emb_dropout=0.1
-    )
+    elif vit_num == 3:
+        model = vit2.ViT(
+            image_size=img_size,
+            patch_size=32,
+            num_classes=1000,
+            dim=1024,
+            depth=6,
+            heads=16,
+            mlp_dim=2048,
+            dropout=0.1,
+            emb_dropout=0.1
+        )
 
     optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
     # optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
@@ -129,4 +133,10 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--batch', type=int, default=32)
+    parser.add_argument('--epoch', type=int, default=5)
+    parser.add_argument('--img', type=int, default=256)
+    parser.add_argument('--vit', type=int, default=1)
+    opt = parser.parse_args()
+    main(opt)
